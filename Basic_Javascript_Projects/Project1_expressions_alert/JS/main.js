11a50d1878ed3556595b9f82a50c78a7d7a8a9d9
J Rockwell Seebach <105180010+SavageElements@users.noreply.github.com>
2022-06-06 02:14:34 -0700

    added first round of files

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,29 @@
+window.alert ("Hello, World!");
+
+document.write('Hello, World!')
+
+var X = "Hello, World!"
+
+document.write (X);
+
+let welcome = "Welcome to Twin Peaks"
+
+let room = 4
+
+window.alert (room)
+
+let crazy = "Frank Zappa\'s Apostrophy was \"crazy\"!"
+
+document.write(crazy)
+
+document.write (room + crazy)
+
+let duce = (crazy + welcome)
+document.write (duce)
+
+let amigos = "The Three Amigos", short = "Ned", chase ="Dusty", martin = "Lucky"
+document.write (short)
+
+document.write (9+4)
+
+console.log ()
